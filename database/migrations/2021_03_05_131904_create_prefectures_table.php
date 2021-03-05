4f04ba860cd6206424c5d7c6c4ab85ffb2712caa
tabayoshi <tabayoshi53@gmail.com>
2021-03-05 22:58:39 +0900

    first commit

---
+++ database/migrations/2021_03_05_131904_create_prefectures_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrefecturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prefectures', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('mountain_id');
+            $table->string('prefecture_name', 5);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prefectures');
+    }
+}
