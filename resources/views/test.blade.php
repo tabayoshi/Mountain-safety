9f6e3cebabae8d0b4e871ac1f1a83ffe406ddd87
oga559 <coolsky.4649@gmail.com>
2021-03-07 15:38:04 +0900

    投稿ページ飛べないです

---
+++ resources/views/test.blade.php
@@ -0,0 +1,2 @@
+仮想一覧ページ
+<a href="{{ route('post.create') }}">投稿ページ</a>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('test');
 });
+
+
+//投稿画面と処理のルート
+Route::resource('post', PostController::class)->only([
+    'create',
+    ]);
